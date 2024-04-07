2694017ce34b25916acead59b1f580da9fbaf27c
Claire <43319552+ClaireRimel@users.noreply.github.com>
2024-04-07 19:16:31 +0200

    New: create App project

---
+++ FocusTV/FocusTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTVApp.swift
+//  FocusTV
+//
+//  Created by Claire on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FocusTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
